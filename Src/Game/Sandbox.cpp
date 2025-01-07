da98d114c6080ce94970563e3d14e11b5c67797f
JZNMCO <JZNMCO@55.com>
2025-01-07 22:14:25 +0800

    add transform to renderer

--- Src/Game/Sandbox.cpp
+++ Src/Game/Sandbox.cpp
@@ -35,10 +35,10 @@ public:
         // Squares
         mpBlueVertexArray.reset(Orange::VertexArray::Create());
         float squareVertices[3 * 4] = {
-            -0.75f, -0.75f, 0.0f,
-             0.75f, -0.75f, 0.0f,
-             0.75f,  0.75f, 0.0f,
-            -0.75f,  0.75f, 0.0f
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.5f,  0.5f, 0.0f,
+            -0.5f,  0.5f, 0.0f
         };
 
         std::shared_ptr<Orange::VertexBuffer> squareVB;
@@ -59,6 +59,7 @@ public:
             layout(location = 0) in vec3 a_Position;
             layout(location = 1) in vec4 a_Color;
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             out vec3 v_Position;
             out vec4 v_Color;
@@ -67,7 +68,7 @@ public:
             {
                 v_Position = a_Position;
                 v_Color = a_Color;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);    
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -93,12 +94,14 @@ public:
             
             layout(location = 0) in vec3 a_Position;
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
+
             out vec3 v_Position;
 
             void main()
             {
                 v_Position = a_Position;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);    
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -156,10 +159,19 @@ public:
         mpCamera->SetRotation(mCameraRotation);
 
         Orange::Renderer::BeginScene(mpCamera);
-        mpBlueShader->Bind();
-        Orange::Renderer::Submit(mpBlueShader, mpBlueVertexArray);
+        
+        glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
-        mpShader->Bind();
+        for (int y = 0; y < 20; y++)
+        {
+            for (int x = 0; x < 20; x++)
+            {
+                glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+                glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+                Orange::Renderer::Submit(mpBlueShader, mpBlueVertexArray, transform);
+            }
+        }
+
         Orange::Renderer::Submit(mpShader, mpVertexArray);
         Orange::Renderer::EndScene();
     }
--- Src/OrangeEngine/Engine/Renderer/Renderer.cpp
+++ Src/OrangeEngine/Engine/Renderer/Renderer.cpp
@@ -16,10 +16,13 @@ namespace Orange
     {
     }
 
-    void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+    void Renderer::Submit(const std::shared_ptr<Shader>& shader,
+        const std::shared_ptr<VertexArray>& vertexArray, 
+        const glm::mat4& transform)
     {
         shader->Bind();
         shader->UploadUniformMat4("u_ViewProjection", mpSceneData->ViewProjectionMatrix);
+        shader->UploadUniformMat4("u_Transform", transform);
         vertexArray->Bind();
         RenderCommand::DrawIndexed(vertexArray);
     }
