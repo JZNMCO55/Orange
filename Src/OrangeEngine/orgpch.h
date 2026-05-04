fa375ea139454ac385bc0e60479bd85ca66206a8
JZNMCO <jznmco@hotmail.com>
2026-05-04 18:06:26 +0800

    删除不必要的脚本文件

--- Src/OrangeEngine/orgpch.h
+++ Src/OrangeEngine/orgpch.h
@@ -25,8 +25,4 @@
 #include <filesystem>
 #include <thread>
 
-#include "Core/Base/Logger.h"
-#include "Core/Base/Timer.h"
-
-
 #endif // ORG_PCH_H
--- script/GenerateWithReleaseStatic.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: 下载并构建 Release 版本的三方库
-call downloadDepndence.bat Release
-
-:: 调用主构建脚本进行 Release 配置的构建
-call generate.bat Release static
